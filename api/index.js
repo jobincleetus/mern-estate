2975ad6c91751e7146fdf41f3d9a1bbd61ed96e4
Jobin Cleetus <jobincleetus5@gmail.com>
2023-12-06 17:37:17 +0530

    feat: backend with db

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB)
+    .then(() => console.log('Connected!'))
+    .catch((err) => console.log(err));
+
+const app = express();
+
+app.listen('3000', () => {
+    console.log('App is running')
+})
