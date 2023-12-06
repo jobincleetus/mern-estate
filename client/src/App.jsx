dae124711186af4f7ddb57b910076a0f1fc30fe0
Jobin Cleetus <jobincleetus5@gmail.com>
2023-12-06 15:48:39 +0530

    feat: base pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Sign-in" element={<SignIn />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
