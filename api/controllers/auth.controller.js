28daea4def355defbf3ed938ca8b010d20059d5e
Jobin Cleetus <jobincleetus5@gmail.com>
2023-12-15 12:39:45 +0530

    feat: middleware and utils error handling function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,14 @@
 import bcrypt from "bcryptjs/dist/bcrypt.js";
 import User from "../models/user.model.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const {userName, emailId, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const userData = new User({userName, emailId, password: hashedPassword});
     try {
         const response = await userData.save();
-        console.log(response)
         res.status(200).json('User created successfully')
     } catch (err) {
-        res.status(500).json(err.message)
+        next(err);
     }
 }
--- api/index.js
+++ api/index.js
@@ -19,3 +19,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    });
+});
